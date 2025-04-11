f2177b39e81a04e3f8a23b818b4107b479855d4d
Anaholcman <anitaholcman@gmail.com>
2025-04-11 11:15:34 -0300

    dd

--- TP2-x86_64/ej1/ej1.c
+++ TP2-x86_64/ej1/ej1.c
@@ -3,7 +3,7 @@
 string_proc_list* string_proc_list_create(void){
 	string_proc_list *list = malloc(sizeof(string_proc_list));
 	if (!list) {
-		return NULL;
+		exit(EXIT_FAILURE);
 	}
 	list->first = NULL;
 	list->last  = NULL;
@@ -14,7 +14,7 @@ string_proc_node* string_proc_node_create(uint8_t type, char* hash){
 	string_proc_node *node = malloc(sizeof(string_proc_node));
 	if (!node) {
 		// no se hizo el malloc
-		return NULL;
+		exit(EXIT_FAILURE);
 	}
 	node->next      = NULL;
 	node->previous  = NULL;

624be4ff301c4125241e2ff186b6f9d8b76c384c
Anaholcman <anitaholcman@gmail.com>
2025-04-13 11:05:36 -0300

    b

--- TP2-x86_64/ej1/testa.c
+++ TP2-x86_64/ej1/testa.c
@@ -2,18 +2,9 @@
 #include "ej1.h"
 
 int main() {
-    string_proc_list* lista = string_proc_list_create_asm();
+    void* lista = string_proc_list_create_asm();
 
-    printf("🧪 Dirección devuelta por string_proc_list_create_asm: %p\n", (void*)lista);
-
-    if (!lista) {
-        printf("❌ ERROR: lista es NULL\n");
-        return 1;
-    }
-
-    printf("✅ Lista creada correctamente.\n");
-    printf("   list->first: %p\n", (void*)lista->first);
-    printf("   list->last:  %p\n", (void*)lista->last);
+    printf("🧪 Dirección devuelta por ASM: %p\n", lista);
 
     return 0;
 }

42ef000ff344ea8e60af373d83c95545143e7bce
Anaholcman <anitaholcman@gmail.com>
2025-05-09 11:58:42 -0300

    Update file.c

--- TP3-FileSystem/file.c
+++ TP3-FileSystem/file.c
@@ -33,7 +33,6 @@ int file_getblock(struct unixfilesystem *fs, int inumber, int blockNum, void *bu
     } else {
         valid_bytes = BLOCK_SIZE;
     }
-    printf("file_getblock: blockNum %d → physical block %d\n", blockNum, block_disc_index);
 
     return valid_bytes;
 }

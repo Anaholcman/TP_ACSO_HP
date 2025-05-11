5331317d76cf743277226bdc05ec64ab78a2b087
Anaholcman <anitaholcman@gmail.com>
2025-05-11 15:15:28 -0300

    psth

--- TP3-FileSystem/pathname.c
+++ TP3-FileSystem/pathname.c
@@ -7,7 +7,25 @@
 #include <string.h>
 #include <assert.h>
 
+#define ROOT_INODE 1
 
 int pathname_lookup(struct unixfilesystem *fs, const char *pathname) {
-	return 0;
+	int inumber = ROOT_INODE;
+
+    char path_copy[strlen(pathname) + 1];
+    strcpy(path_copy, pathname);
+
+    
+    char *token = strtok(path_copy, "/");
+    while (token != NULL) {
+        struct direntv6 entry;
+        int dir_name = directory_findname(fs, token, inumber, &entry);
+        if (dir_name < 0) {
+            return -1;  
+        }
+        inumber = entry.d_inumber;
+        token = strtok(NULL, "/");
+    }
+
+    return inumber;
 }

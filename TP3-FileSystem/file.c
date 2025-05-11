eb9a394b72d97f99dff1692232b986f84e3d993f
Anaholcman <anitaholcman@gmail.com>
2025-05-11 15:01:53 -0300

    0

--- TP3-FileSystem/file.c
+++ TP3-FileSystem/file.c
@@ -15,8 +15,11 @@ int file_getblock(struct unixfilesystem *fs, int inumber, int blockNum, void *bu
     int inode_size = inode_getsize(&inode_struct);
     int max_blocks = (inode_size + BLOCK_SIZE-1) / BLOCK_SIZE;
 
-    if (blockNum < 0 || blockNum >=max_blocks){
+    if (blockNum < 0){
         return -1;
+    }
+    if (blockNum >= max_blocks){
+        return 0;
     }
 
     int block_disc_index = inode_indexlookup(fs, &inode_struct, blockNum);
